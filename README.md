c7567ef13f12350c4003258ebc2d3e08f6e2f22e
S.Yanchevsky <s.yanchevsky@spheremall.com>
2018-10-29 16:30:28 +0200

    base code

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# mgateway
+# GateWay
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This is file for example base code!
+ *
+ * Created by PhpStorm.
+ * User: shaman
+ * Date: 10/29/18
+ * Time: 4:07 PM
+ */
+
+
+echo "OK";
